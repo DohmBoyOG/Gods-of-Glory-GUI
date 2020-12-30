42707ccda9eef1f3402a784d153f8251be2fdcd7
Tek9x <sean.schramm@live.com>
2020-12-30 05:37:52 -0500

    ffff

---
+++ Menus/World.lua
@@ -0,0 +1,36 @@
+worlds = game:GetService("Workspace").GameWorlds
+remote = game:GetService("ReplicatedStorage").Remotes.BreakBuilding
+
+local config = {
+   ["HeaderWidth"] = 200,
+   ['PrimaryColor'] = Color3.fromRGB(166, 167, 159),
+   ['SecondaryColor'] = Color3.fromRGB(73, 103, 123),
+   ["AccentColor"] = Color3.new(0.6,0,0)
+}
+
+--- GUI Setup ---
+local gui = loadstring(game:HttpGet("https://gitlab.com/0x45.xyz/droplib/-/raw/master/drop-minified.lua"))():Init(config,game.CoreGui)
+local lpg = gui:CreateCategory("World Fun Options ")
+--- GUI Setup ---
+
+local sDestruct = lpg:CreateButton('Self Destruct Worlds',function() spawn(function()
+    for _, v in pairs(worlds:GetChildren()) do
+        if v:IsA("Folder") then
+            for _, destructs in pairs(v:GetDescendants()) do
+                if destructs:IsA("Folder") and destructs.Name == "Destructibles" then
+                    for _, begin in pairs(destructs:GetChildren()) do
+                        remote:FireServer(
+                            {
+                                ["TagFullName"] = tostring(begin:GetFullName())
+                                
+                            }
+                            )
+                    end
+                end
+            end
+        end
+    end
+end
+)
+--local Exit = lpg:CreateButton("Exit",function() gui:CleanUp() end) 
+end)
