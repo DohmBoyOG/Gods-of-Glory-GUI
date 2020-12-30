e9a3438dc7c3066faa4f3cf9cc6f406b28ae7324
Tek9x <sean.schramm@live.com>
2020-12-30 03:58:57 -0500

    Update Player.lua

--- Menus/Player.lua
+++ Menus/Player.lua
@@ -2,7 +2,9 @@ thisPlayer = game:GetService("Players").LocalPlayer
 otherPlayer = game:GetService("Players")
 
 local remote = game:GetService("ReplicatedStorage").Remotes.KickOtherPlayer
-
+local rageMode
+local customwait
+local noOptions = true
 
 local config = {
    ["HeaderWidth"] = 200,
@@ -17,7 +19,10 @@ local lpg = gui:CreateCategory("Player Options ")
 --- GUI Setup ---
 
 --- Button Setup --
-local kickALL = lpg:CreateButton('Kick All Players', function() kicker() end)
+local kickOptions = lpg:CreateSection('Kicker Options')
+local kickALL = kickOptions:CreateButton('Kick All Players', function() kicker() end)
+local rage = kickOptions:CreateSwitch('Rage Mode', function(bool) rageMode = bool end)
+local rage_slider = kickOptions:CreateSlider("Rage Mode Wait", function(value) noOptions = false customWait = value end,0,10,0.1,false,1)
 
 
 --- Functions Setup ---
@@ -32,6 +37,31 @@ function kicker()
             end
         end
     end
+
+
+function rageStart()
+    for _, v in pairs(otherPlayer:GetPlayers()) do
+        if v ~= thisPlayer then
+            remote:FireServer({
+                ["OtherCharacter"] = v.Character,
+                ["LV"] = Vector3.new(math.random(5000), math.random(5000), math.random(5000))
+                })
+            end
+        end
+    end
+
+while wait() do
+    if rageMode == true and noOptions == true then
+        spawn(rageStart)
+        print('nowait')
+    elseif rageMode == true and customWait > 0 then
+        wait(customWait)
+        spawn(rageStart)
+        print('waittt')
+    else
+        noOptions = true
+    end
+end
 
 
 
